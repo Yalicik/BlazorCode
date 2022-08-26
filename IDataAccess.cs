b634fc606193e929242fe432614061bf479d46a2
Yalicik <112151765+Yalicik@users.noreply.github.com>
2022-08-26 09:09:02 +0300

    IDataAccess.cs

---
+++ IDataAccess.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DataLibary
+{
+    public interface IDataAccess
+    {
+        Task<List<T>> LoadData<T, U>(string sql, U parameters, string connectionString);
+
+        Task SaveData<T>(string sql, T parameters, string connectionString);
+    }
+}
